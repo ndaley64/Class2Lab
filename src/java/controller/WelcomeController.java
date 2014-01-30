1c04c64959f76ff6cf4c1112f7d61cac03bc86ea
ndaley <ndaley@L11106038622.wctc.msft>
2014-01-30 12:19:42 -0600

    Tiny bit of fixing.

--- src/java/controller/WelcomeController.java
+++ src/java/controller/WelcomeController.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,8 +15,6 @@ import model.WelcomeService;
  */
 @WebServlet(name = "welcomeController", urlPatterns = {"/greeter"})
 public class WelcomeController extends HttpServlet {
-
-    private static final String RESULT_PAGE = "result.jsp";
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -53,25 +44,15 @@ public class WelcomeController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
 
-        // parameters are name attributes in view pages
-        // Here we're retrieving form content from form.html
         String name = request.getParameter("firstName");
 
-        // Create a new instance of a model object
-        // For some applications, we would not want to create a new one each time.
         WelcomeService ws = new WelcomeService();
-        // Always a good idea to trim and/or validate input data
         String welcome = ws.getWelcomeMessage(name);
 
-        // Parameters are read only Request object properties, but attributes
-        // are read/write. We can use attributes to store data for use on
-        // another page.
         request.setAttribute("welcomeMessage", welcome);
 
-        // This object lets you forward both the request and response
-        // objects to a destination page
         RequestDispatcher view
-                = request.getRequestDispatcher(RESULT_PAGE);
+                = request.getRequestDispatcher("result.jsp");
         view.forward(request, response);
     }
 
